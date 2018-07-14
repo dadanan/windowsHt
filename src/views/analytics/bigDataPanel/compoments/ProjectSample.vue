49364d5edb3882ed836ec2bb822324429347c702
ヌルポ <episodes27@gmail.com>
2018-07-14 13:10:35 +0800

    feat: 添加了工程案列基本数据（弹窗+设置实现)

---
+++ src/views/analytics/bigDataPanel/compoments/ProjectSample.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-row class="project-samples">
+    <el-col v-for="project in data" :key="project.id" :span="6">
+      <span>{{project.icon}}</span>
+      <p>{{project.name}}</p>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+    export default {
+      props: ['options'],
+      data() {
+        return {
+          data: null
+        }
+      },
+      created() {
+        this.populateData()
+      },
+      methods: {
+        populateData() {
+          this.data = this.options.data
+        }
+      }
+    }
+</script>
+
+<style lang="scss" scoped>
+  .project-samples {
+    text-align: center;
+  }
+</style>
