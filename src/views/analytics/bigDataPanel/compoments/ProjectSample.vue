e2e9960c7019f7c2de08a861b5f2da84464373af
ヌルポ <episodes27@gmail.com>
2018-07-14 13:22:54 +0800

    feat: 添加了设置按钮

--- src/views/analytics/bigDataPanel/compoments/ProjectSample.vue
+++ src/views/analytics/bigDataPanel/compoments/ProjectSample.vue
@@ -1,5 +1,7 @@
 <template>
   <el-row class="project-samples">
+    <a class="setting" type="primary" size="mini">设置</a>
+    <h3>工程案例</h3>
     <el-col v-for="project in data" :key="project.id" :span="6">
       <span>{{project.icon}}</span>
       <p>{{project.name}}</p>
@@ -29,5 +31,11 @@
 <style lang="scss" scoped>
   .project-samples {
     text-align: center;
+    margin-left: 10px;
+    margin-right: 10px;
+    
+  }
+  .setting{
+    float: right;
   }
 </style>
