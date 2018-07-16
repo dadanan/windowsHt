f0b74141a6905ac2387f93e8f7332f52fca4241d
ovo4096 <ovo4096@gmail.com>
2018-07-17 01:12:57 +0800

    feat: 添加设备的几个空页面

---
+++ src/views/device/filing.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="dashboard-container">设备备案</div>
+</template>
---
+++ src/views/device/list.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="dashboard-container">
+    <el-card>
+      <div class="table-opts">
+        <el-button-group>
+          <el-button type="primary">分配</el-button>
+          <el-button type="primary">导入</el-button>
+          <el-button type="primary">添加</el-button>
+          <el-button type="primary">删除</el-button>
+          <el-button type="primary">解绑</el-button>
+          <el-button type="primary">禁用</el-button>
+          <el-button type="primary">集群</el-button>
+          <el-button type="primary">自定义</el-button>
+        </el-button-group>
+      </div>
+      <el-table
+        :data="deviceList"
+        style="width: 100%" class="mb20" border>
+        <el-table-column type="selection"></el-table-column>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column
+          prop="name"
+          label="名称" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column
+          prop="mac"
+          label="MAC" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column
+          prop="owner"
+          label="归属" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column
+          prop="type"
+          label="类型" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column
+          prop="isBound"
+          label="绑定状态" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column
+          prop="isEnabled"
+          label="启用状态" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column
+          prop="clusterID"
+          label="集群 ID" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column
+          prop="clusterName"
+          label="集群名" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column
+          prop="isBoot"
+          label="工作状态" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column
+          prop="isOnline"
+          label="在线状态" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button type="text">编辑</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="excel-container">
+        <el-pagination
+          :current-page="1"
+          :page-sizes="[100, 200, 300, 400]"
+          :page-size="100"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="400">
+        </el-pagination>
+        <el-button type="primary">导出 Excel</el-button>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      const deviceList = []
+
+      for (let i = 0; i < 15; i++) {
+        deviceList.push({
+          name: '测试设备',
+          mac: '25-14-B1-75-49-EB',
+          owner: '测试用户',
+          type: '测试类型',
+          isBound: '绑定',
+          isEnabled: '禁用',
+          clusterID: '2564',
+          clusterName: '测试集群',
+          isBoot: '关机',
+          isOnline: '离线',
+          // 以下非默认显示
+          deviceID: '',
+          deviceModel: '',
+          deviceName: '',
+          regDatetime: '',
+          lastOnlineDatetime: '',
+          bindUser: '',
+          position: ''
+        })
+      }
+      return {
+        deviceList
+      }
+    }
+  }
+</script>
