c3d996f5ece34f09040ee028b55be5a528e5f53c
Eric Xue <ovo4096@gmail.com>
2018-07-19 04:07:53 +0800

    feat: 修改组件命名

---
+++ src/views/device/components/DeviceClusterDialog.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-dialog title="设备集群" :visible="visible" @update:visible="$emit('update:visible', $event)"></el-dialog>
+</template>
+
+<script>
+  export default {
+    props: {
+      visible: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
