7e8ea722446d13f240baa41aa11dea7347004c53
Eric Xue <ovo4096@gmail.com>
2018-07-20 08:14:09 +0800

    feat: 设备备案添加对话框

---
+++ src/views/device/components/FilingAddDialog.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-dialog title="设备备案创建" :visible="visible" @update:visible="$emit('update:visible', $event)">
+    <el-form label-width="100px" label-position="left">
+      <el-form-item label="Type ID">
+        <el-select v-model="placeholder">
+          <el-option label="Placeholder1" value="1"></el-option>
+          <el-option label="Placeholder2" value="2"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="设备默认名称">
+        <el-input v-model="placeholder"></el-input>
+      </el-form-item>
+      <el-form-item label="设备默认型号">
+        <el-input v-model="placeholder"></el-input>
+      </el-form-item>
+      <el-form-item label="来源">
+        <el-select v-model="placeholder">
+          <el-option label="Placeholder1" value="1"></el-option>
+          <el-option label="Placeholder2" value="2"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="设备默认缩图">
+        <el-button type="primary">上传文件</el-button>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="addDeviceDialogVisible = false">取消</el-button>
+      <el-button type="primary" @click="addDevice">确定</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+  import DTitle from '@/components/Title'
+
+  export default {
+    components: {
+      DTitle
+    },
+    props: {
+      visible: {
+        type: Boolean,
+        default: false
+      }
+    },
+    data() {
+      return {
+        placeholder: 'placeholder'
+      }
+    }
+  }
+</script>
--- src/views/device/filing.vue
+++ src/views/device/filing.vue
@@ -1,3 +1,84 @@
 <template>
-  <div class="dashboard-container">设备备案</div>
+  <div class="dashboard-container">
+    <el-card>
+      <div class="table-opts">
+        <el-button-group>
+          <el-button type="primary" @click="filingAddDialogVisible = true">新建</el-button>
+        </el-button-group>
+      </div>
+      <el-table
+        :data="typeList"
+        style="width: 100%" class="mb20" border>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column
+          prop="placeholder"
+          label="品名" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column
+          prop="placeholder"
+          label="型号" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column
+          prop="placeholder"
+          label="typeID" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column
+          prop="placeholder"
+          label="功能项" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column
+          prop="placeholder"
+          label="创建人" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column
+          prop="placeholder"
+          label="创建日期" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column
+          prop="placeholder"
+          label="来源" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column
+          prop="placeholder"
+          label="归属" show-overflow-tooltip sortable>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button type="text">编辑</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        :current-page="1"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400">
+      </el-pagination>
+    </el-card>
+    <filing-add-dialog :visible.sync="filingAddDialogVisible"></filing-add-dialog>
+  </div>
 </template>
+
+<script>
+  import FilingAddDialog from './components/FilingAddDialog'
+
+  export default {
+    components: {
+      FilingAddDialog
+    },
+    data() {
+      const typeList = []
+
+      for (let i = 0; i < 15; i++) {
+        typeList.push({
+          placeholder: 'placeholder'
+        })
+      }
+      return {
+        typeList,
+        filingAddDialogVisible: false
+      }
+    }
+  }
+</script>
